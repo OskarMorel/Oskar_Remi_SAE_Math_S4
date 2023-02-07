9df0ad4ededbfad4eaff85699f118867c7ff3d6d
REMI JAUZION <remi.jauzion@iut.local>
2023-02-07 10:34:41 +0100

    lecture fichier

--- RegressionLineaireResolutionAnalytique.py
+++ RegressionLineaireResolutionAnalytique.py
@@ -1,4 +1,13 @@
 # | Auteurs : Remi Jauzion - Oskar Morel |
 # |-------------------------------------|
-# Algorithme N°1 pour l'estimation du prix d'un appartment  par rapport à ça surface  Régression linéaire par resolution analytique
+# Algorithme N°1 pour l'estimation du prix d'un appartement  par rapport à ça surface  Régression linéaire par resolution analytique
 
+with open("values.txt", "r") as f:
+    x = list()
+    y = list()
+    for line in f:
+        data = line.split()
+        x.append(data[0])
+        y.append(data[1])
+print(x)
+print(y)
