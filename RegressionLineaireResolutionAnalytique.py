f84684c9d02d70b76f36a670b2b21c3613c31508
MorelOskar <103489695+OskarMorel@users.noreply.github.com>
2023-02-07 11:13:04 +0100

    Partie 1 regression linéaire méthode analytique

--- RegressionLineaireResolutionAnalytique.py
+++ RegressionLineaireResolutionAnalytique.py
@@ -2,16 +2,23 @@
 # |-------------------------------------|
 # Algorithme N°1 pour l'estimation du prix d'un appartement  par rapport à ça surface  Régression linéaire par resolution analytique
 
-with open("values.txt", "r") as f:
-    x = list()
-    y = list()
-    for line in f:
-        try:
-            line = line.strip()
-            data = line.split(chr(9))
-            x.append(data[0])
-            y.append(data[1])
-        except:
-            print("Erreur lecture fichier")
-            x.clear
-            y.clear
+
+from statistics import variance
+from statistics import covariance
+from statistics import mean
+
+x = [32, 50, 21, 23]
+y = [36.3, 5, 21.6, 22]
+
+def trouverA(x, y):
+    return covariance(x, y) / variance(x)
+
+def trouverB(x, y, a):
+    return mean(y) - a * mean(x)
+
+def regressionLineaire(x, y):
+    a = trouverA(x, y)
+    b = trouverB(x, y, trouverA(x, y))
+    return "a = " + str(a) + "\nb = " + str(b)
+
+print(regressionLineaire(x,y))
