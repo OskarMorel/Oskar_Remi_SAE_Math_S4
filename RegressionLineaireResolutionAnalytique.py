18852f4c894b57f02a873821e4fcd80a0d01d717
MorelOskar <103489695+OskarMorel@users.noreply.github.com>
2023-02-07 09:29:55 +0100

    Premier commit

---
+++ RegressionLineaireResolutionAnalytique.py
@@ -0,0 +1,4 @@
+# | Auteurs : Remi Jauzion - Oskar Morel |
+# |-------------------------------------|
+# Algorithme N°1 pour l'estimation du prix d'un appartment  par rapport à ça surface  Régression linéaire par resolution analytique
+
