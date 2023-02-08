7df23160d80cae1145f47dfe4b4f5846aea7e028
MorelOskar <103489695+OskarMorel@users.noreply.github.com>
2023-02-08 11:57:16 +0100

    deux parties finies

--- RegressionLineaireDescenteGradient.py
+++ RegressionLineaireDescenteGradient.py
@@ -2,9 +2,6 @@
 # |-------------------------------------|
 # Algorithme N°1 pour l'estimation du prix d'un appartement  par rapport à ça surface  Régression linéaire par descente de gradient
 
-from RegressionLineaireResolutionAnalytique import trouverA
-from RegressionLineaireResolutionAnalytique import trouverB
-
 with open("values.txt", "r") as f:
     x = list()
     y = list()
@@ -19,14 +16,43 @@ with open("values.txt", "r") as f:
             x.clear
             y.clear
 
-a = trouverA(x, y)
-b = trouverB(x, y, trouverA(x, y))
-deriveF = a
+def derivePartielleA(x,y,a,b):
+    sommeXAuCarre = 0
+    sommeX = 0
+    sommeXY = 0
+    for i in range(len(x)):
+        sommeXAuCarre = sommeXAuCarre + (x[i] * x[i])
+        sommeX = sommeX + x[i]
+        sommeXY = sommeXY + (x[i] * y[i])
 
-def f(x):
-    return a * x + b
+    resultat = 2 * ((a * sommeXAuCarre) + (b * sommeX) - sommeXY)
+    return resultat
 
-# La derivée est une constante donc on est pas sensé faire une itération ça sert à rien mais du coup
-# je vais voir comment faut faire
+def derivePartielleB(x,y,a,b):
+    sommeX = 0
+    sommeY = 0
+    for i in range(len(x)):
+        sommeX = sommeX + x[i]
+        sommeY = sommeY + y[i]
+
+    resultat = 2 * ((a * sommeX) + (len(x) * b) - sommeY)
+    return resultat
+
+def descenteGradient(x,y,a,b):
+    pas = 0.00001
+    fini = False
+    while not fini:
+
+        if abs(derivePartielleB(x,y,a,b)) <= 0.0001 and abs(derivePartielleA(x,y,a,b)) <= 0.0001:
+            fini = True
+
+        b -= derivePartielleB(x,y,a,b) * pas
+        a -= derivePartielleA(x,y,a,b) * pas
+
+    return "gradient : a = " + str(a) + " | b = " + str(b)
+
+print(descenteGradient(x,y,1,1))
+
+
 
 
